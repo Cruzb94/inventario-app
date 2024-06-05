efc55f48f9deb577292a650a380c6ea1703d5997
Douglas Romero <douglasjhoenky@gmail.com>
2024-06-04 21:16:26 -0400

    TODOLISTO25

--- resources/views/salidas/create.blade.php
+++ resources/views/salidas/create.blade.php
@@ -107,134 +107,6 @@
 
 {{ Form::close() }}
 
-<script>
-    let referenciaIndex = 1;
-    function addReferencia() {
-    const container = document.getElementById('referencias-container');
-    const div = document.createElement('div');
-    div.classList.add('form-group', 'row', 'mt-5', 'referencia-item');
-
-    /*const currentIndex = document.querySelectorAll('.referencia-item').length + 1;
-
-    const hr = document.createElement('hr');
-    hr.classList.add('my-4');
-    container.appendChild(hr);*/
-
-    div.innerHTML = `
-        <div class="d-flex justify-content-between mb-5 ">
-            
-          
-            <button type="button" class="btn btn-danger" onclick="removeReferencia(this)">
-                <i class="fa-solid fa-circle-xmark"></i>
-            </button>            
-        </div>
-        <div class="col-sm-2 position-relative">
-            {{ Form::label('referencia[]', 'Referencia ', ['class'=>'form-label']) }} 
-            {{ Form::select('referencia[]', $productos->pluck('referencia', 'referencia'), null, ['class' => 'form-control', 'placeholder' => 'Seleccione una referencia','onchange' => 'updateDescripcion(this)']) }}
-        </div>
-        <div class="col-sm-2 position-relative">
-            {{ Form::label('descripcion[]', 'Descripcion ', ['class'=>'form-label']) }}
-            {{ Form::text('descripcion[]', null, ['class' => 'form-control', 'readonly' => true]) }}
-        </div>
-        <div class="col-sm-2 ">
-            {{ Form::label('cantidad[]', 'Cantidad', ['class'=>'form-label']) }}
-            {{ Form::number('cantidad[]', null, ['class' => 'form-control', 'oninput' => 'calculateTotal(this)']) }}
-        </div>
-        <div class="col-sm-2">
-            {{ Form::label('valor[]', 'Valor', ['class'=>'form-label']) }}
-            {{ Form::number('valor[]', null, ['class' => 'form-control', 'oninput' => 'calculateTotal(this)']) }}
-        </div>
-        <div class="col-sm-2">
-            {{ Form::label('valortotal[]', 'ValorTotal', ['class'=>'form-label']) }}
-            {{ Form::number('valortotal[]', null, ['class' => 'form-control', 'readonly' => true]) }}
-         </div>
-    `;
-    container.appendChild(div);
-
-
-    
-}
-function removeReferencia(button) {
-    const referenciaItem = button.closest('.referencia-item');
-    const hr = referenciaItem.previousElementSibling; // Obtener el hr anterior al grupo de referencia
-    referenciaItem.remove();
-
-    updateTotalValue(); // Actualiza el total después de eliminar una referencia
-    updateTotalCantidad();
-
-   /* if (hr && hr.classList.contains('my-4')) {
-        hr.remove(); // Eliminar el hr si existe y tiene la clase 'my-4'
-    }*/
-   // updateReferenciaLabels(); // Llama a esta función para actualizar los números de referencia
-}
-
-function updateReferenciaLabels() {
-    const items = document.querySelectorAll('.referencia-item');
-    let currentIndex = 2;
-    items.forEach((item) => {
-        const h3 = item.querySelector('h3');
-        if (h3) {
-            h3.textContent = `Referencia ${currentIndex}`;
-            currentIndex++;
-        }
-    });
-}
-
-</script>
-
-<script>
-    function calculateTotal(element) {
-    const referenciaItem = element.closest('.referencia-item');
-    const cantidad = referenciaItem.querySelector('input[name="cantidad[]"]').value;
-    const valor = referenciaItem.querySelector('input[name="valor[]"]').value;
-    const totalField = referenciaItem.querySelector('input[name="valortotal[]"]');
-    const total = cantidad * valor;
-    totalField.value = total ? total.toFixed(2) : 0;
-    // Actualizar el valor de "Valor de todo"
-    updateTotalValue();
-    updateTotalCantidad();
-}
-
-function updateTotalValue() {
-    const totalFields = document.querySelectorAll('input[name="valortotal[]"]');
-    let totalSum = 0;
-    
-    totalFields.forEach(field => {
-        totalSum += parseFloat(field.value) || 0;
-    });
-    
-    const totalTodoField = document.querySelector('input[name="valortodo"]');
-    totalTodoField.value = totalSum.toFixed(2);
-}
-function updateTotalCantidad() {
-    const cantidadFields = document.querySelectorAll('input[name="cantidad[]"]');
-    let totalCantidad = 0;
-
-    cantidadFields.forEach(field => {
-        totalCantidad += parseFloat(field.value) || 0;
-    });
-
-    const totalCantidadField = document.querySelector('input[name="valorcantidadtotal"]');
-    totalCantidadField.value = totalCantidad.toFixed(2);
-}
-</script>
-<script>
-        // Almacena las descripciones de los productos en un objeto
-        const productos = @json($productos);
-
-function updateDescripcion(selectElement) {
-    // Obtener el valor seleccionado
-    const selectedReferencia = selectElement.value;
-
-    // Buscar la descripción correspondiente
-    const producto = productos.find(producto => producto.id == selectedReferencia);
-    const descripcion = producto ? producto.descripcion : '';
-
-    // Obtener el campo de descripción correspondiente
-    const descripcionField = selectElement.closest('.referencia-item').querySelector('input[name="descripcion[]"]');
-    descripcionField.value = descripcion;
-}
-</script>
 @stop
 
 @section('css')
@@ -277,6 +149,209 @@ function updateDescripcion(selectElement) {
 
 @section('js')
 
+<script>
+    let referenciaIndex = 1;
+    function addReferencia() {
+    const container = document.getElementById('referencias-container');
+    const div = document.createElement('div');
+    div.classList.add('form-group', 'row', 'mt-5', 'referencia-item');
+
+
+    div.innerHTML = `
+        <div class="d-flex justify-content-between mb-5 ">
+            
+          
+            <button type="button" class="btn btn-danger" onclick="removeReferencia(this)">
+                <i class="fa-solid fa-circle-xmark"></i>
+            </button>            
+        </div>
+        <div class="col-sm-2 position-relative">
+            {{ Form::label('referencia[]', 'Referencia ', ['class'=>'form-label']) }} 
+            {{ Form::select('referencia[]', $productos->pluck('referencia', 'referencia'), null, ['class' => 'form-control', 'placeholder' => 'Seleccione una referencia','onchange' => 'updateDescripcion(this)']) }}
+        </div>
+        <div class="col-sm-2 position-relative">
+            {{ Form::label('descripcion[]', 'Descripcion ', ['class'=>'form-label']) }}
+            {{ Form::text('descripcion[]', null, ['class' => 'form-control', 'readonly' => true]) }}
+        </div>
+        <div class="col-sm-2 ">
+            {{ Form::label('cantidad[]', 'Cantidad', ['class'=>'form-label']) }}
+            {{ Form::number('cantidad[]', null, ['class' => 'form-control', 'oninput' => 'calculateTotal(this)']) }}
+        </div>
+        <div class="col-sm-2">
+            {{ Form::label('valor[]', 'Valor', ['class'=>'form-label']) }}
+            {{ Form::number('valor[]', null, ['class' => 'form-control', 'oninput' => 'calculateTotal(this)']) }}
+        </div>
+        <div class="col-sm-2">
+            {{ Form::label('valortotal[]', 'ValorTotal', ['class'=>'form-label']) }}
+            {{ Form::number('valortotal[]', null, ['class' => 'form-control', 'readonly' => true]) }}
+         </div>
+    `;
+    container.appendChild(div);
+
+
+    
+}
+function removeReferencia(button) {
+    const referenciaItem = button.closest('.referencia-item');
+    const hr = referenciaItem.previousElementSibling; // Obtener el hr anterior al grupo de referencia
+
+    // Obtener el select de referencia
+    const referenciaSelect = referenciaItem.querySelector('select[name="referencia[]"]');
+    // Obtener el valor seleccionado actualmente
+    const selectedReferencia = referenciaSelect.value;
+    // Eliminar la referencia seleccionada del registro
+    const index = referenciasSeleccionadas.indexOf(selectedReferencia);
+    if (index !== -1) {
+        referenciasSeleccionadas.splice(index, 1);
+    }
+
+
+    referenciaItem.remove();
+
+    updateTotalValue(); // Actualiza el total después de eliminar una referencia
+    updateTotalCantidad();
+
+   /* if (hr && hr.classList.contains('my-4')) {
+        hr.remove(); // Eliminar el hr si existe y tiene la clase 'my-4'
+    }*/
+   // updateReferenciaLabels(); // Llama a esta función para actualizar los números de referencia
+}
+
+function updateReferenciaLabels() {
+    const items = document.querySelectorAll('.referencia-item');
+    let currentIndex = 2;
+    items.forEach((item) => {
+        const h3 = item.querySelector('h3');
+        if (h3) {
+            h3.textContent = `Referencia ${currentIndex}`;
+            currentIndex++;
+        }
+    });
+}
+
+</script>
+
+<script>
+    function calculateTotal(element) {
+    const referenciaItem = element.closest('.referencia-item');
+    const cantidadInput = referenciaItem.querySelector('input[name="cantidad[]"]'); 
+    const cantidad = referenciaItem.querySelector('input[name="cantidad[]"]').value;
+    const valor = referenciaItem.querySelector('input[name="valor[]"]').value;
+    const totalField = referenciaItem.querySelector('input[name="valortotal[]"]');
+    const total = cantidad * valor;
+
+    // Obtener el valor seleccionado de la referencia
+    const referenciaSelect = referenciaItem.querySelector('select[name="referencia[]"]');
+    const selectedReferencia = referenciaSelect.value;
+
+    // Obtener el producto correspondiente
+    const producto = productos.find(producto => producto.id == selectedReferencia);
+    const stock = producto ? producto.stock : 0;
+
+    // Verificar si la cantidad supera el stock
+    if (parseInt(cantidad) > stock) {
+        // Mostrar alerta
+        Swal.fire({
+            icon: 'warning',
+            title: 'Advertencia',
+            text: `La cantidad que deseas despachar (${cantidad}) es mayor que la disponible en stock (${stock}) para este producto.`
+        })        // Limpiar el valor de cantidad
+        cantidadInput.value = '';
+        // Salir de la función
+        return;
+    }
+
+
+    totalField.value = total ? total.toFixed(2) : 0;
+    // Actualizar el valor de "Valor de todo"
+    updateTotalValue();
+    updateTotalCantidad();
+}
+
+function updateTotalValue() {
+    const totalFields = document.querySelectorAll('input[name="valortotal[]"]');
+    let totalSum = 0;
+    
+    totalFields.forEach(field => {
+        totalSum += parseFloat(field.value) || 0;
+    });
+    
+    const totalTodoField = document.querySelector('input[name="valortodo"]');
+    totalTodoField.value = totalSum.toFixed(2);
+}
+function updateTotalCantidad() {
+    const cantidadFields = document.querySelectorAll('input[name="cantidad[]"]');
+    let totalCantidad = 0;
+
+    cantidadFields.forEach(field => {
+        totalCantidad += parseFloat(field.value) || 0;
+    });
+
+    const totalCantidadField = document.querySelector('input[name="valorcantidadtotal"]');
+    totalCantidadField.value = totalCantidad.toFixed(2);
+}
+</script>
+<script>
+// Almacena las descripciones de los productos en un objeto
+const productos = @json($productos);
+
+// Registro de referencias seleccionadas
+let referenciasSeleccionadas = [];
+
+function updateDescripcion(selectElement) {
+    // Obtener el valor seleccionado
+    const selectedReferencia = selectElement.value;
+
+    // Verificar si el valor seleccionado ya está en uso
+
+    if (referenciasSeleccionadas.includes(selectedReferencia)) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Advertencia',
+            text: `La referencia ${selectedReferencia} ya ha sido seleccionada.`
+        }) 
+
+const previousSelected = selectElement.dataset.selected;
+//console.log(previousSelected);
+if (previousSelected && previousSelected !== selectedReferencia) {
+
+    //console.log('estoy locococo');
+    // Eliminar la selección anterior si ya no está presente
+    const index = referenciasSeleccionadas.indexOf(previousSelected);
+    if (index !== -1) {
+        referenciasSeleccionadas.splice(index, 1);
+    }
+}
+// Limpiar el campo después de verificar
+selectElement.value = ''; 
+const descripcionField = selectElement.closest('.referencia-item').querySelector('input[name="descripcion[]"]');
+descripcionField.value = ''; // Limpiar el campo de descripción
+return; 
+}
+
+
+
+    // Buscar la descripción correspondiente
+    const producto = productos.find(producto => producto.id == selectedReferencia);
+    const descripcion = producto ? producto.descripcion : '';
+
+        // Eliminar la referencia seleccionada anteriormente que ya no se está utilizando
+        const index = referenciasSeleccionadas.indexOf(selectElement.dataset.selected);
+    if (index !== -1) {
+        referenciasSeleccionadas.splice(index, 1);
+    }
+
+    // Agregar el valor seleccionado al registro
+    referenciasSeleccionadas.push(selectedReferencia)
+
+    // Obtener el campo de descripción correspondiente
+    const descripcionField = selectElement.closest('.referencia-item').querySelector('input[name="descripcion[]"]');
+    descripcionField.value = descripcion;
+
+        // Actualizar el atributo "data-selected" con la nueva referencia seleccionada
+        selectElement.dataset.selected = selectedReferencia;
+}
+</script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         // Obtener el campo de fecha
